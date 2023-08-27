2a03030f07d1029a367b58f280118e01d301570b
Artem Vakhutinskiy <vahut@yandex.ru>
2023-08-27 04:28:37 +0300

    contact, about - done. style done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,172 +4,35 @@ function toggleOpen(hamburger, menu) {
     menu.style.height = hamburger.classList.contains('open') ? '250px' : '0';
 }
 
+// Function to update element classes based on its scrolling and overflow state.
+function setClasses(el) {
+    const isScrollable = el.scrollHeight > el.clientHeight;
+    
+    // GUARD: If element is not scrollable, remove all classes
+    if (!isScrollable) {
+        el.classList.remove('is-bottom-overflowing', 'is-top-overflowing');
+        return;
+    }
+
+    // Check if the user has scrolled to the bottom or top
+    const isScrolledToBottom = el.scrollHeight <= el.clientHeight + el.scrollTop;
+    const isScrolledToTop = el.scrollTop === 0;
+
+    // Apply appropriate classes based on scroll position
+    if (isScrolledToBottom) {
+        el.classList.remove('is-bottom-overflowing');
+        el.classList.add('is-top-overflowing');
+    } else if (isScrolledToTop) {
+        el.classList.remove('is-top-overflowing');
+        el.classList.add('is-bottom-overflowing');
+    } else {
+        // If scrolling neither to top nor bottom, remove both classes
+        el.classList.remove('is-bottom-overflowing', 'is-top-overflowing');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    /*
-    //const wrapper = document.querySelector(".slider-container");
-    const carousel = document.querySelector(".carousel");
-    const firstCardWidth = carousel.querySelector(".portfolio-item").offsetWidth;
-    const arrowBtns = document.querySelectorAll(".slider-container i");
-    //const carouselChildrens = [...carousel.children];
-
-    let isDragging = false;
-
-    const updateButtonVisibility = () => {
-        const isAtFirstElement = carousel.scrollLeft === 0;
-        const isAtLastElement = Math.ceil(carousel.scrollLeft) >= carousel.scrollWidth - carousel.offsetWidth;
-
-        arrowBtns[0].style.display = isAtFirstElement ? "none" : "absolute";
-        arrowBtns[1].style.display = isAtLastElement ? "none" : "absolute";
-    };
-
-    // Dragging of the cards
-    const dragStart = (e) => {
-        isDragging = true;
-        carousel.classList.add("dragging");
-        // Records the initial cursor and scroll position of the carousel
-        startX = e.pageX;
-        startScrollLeft = carousel.scrollLeft;
-    }
-
-    const dragging = (e) => {
-        if(!isDragging) return; // if isDragging is false return from here
-        // Updates the scroll position of the carousel based on the cursor movement
-        updateButtonVisibility();
-        carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-    }
-
-    const dragStop = () => {
-        isDragging = false;
-        carousel.classList.remove("dragging");
-        updateButtonVisibility();
-    }
-
-    updateButtonVisibility(); // Set initial button visibility
-
-    // Add event listeners for the arrow buttons to scroll the carousel left and right
-    arrowBtns.forEach(btn => {
-        btn.addEventListener("click", () => {
-            updateButtonVisibility(); // Update visibility before scrolling
-            carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-            updateButtonVisibility(); // Update visibility after scrolling
-        });
-    });
-
-
-    /*isAutoPlay = true, startX, startScrollLeft, timeoutId;
-
-    // Get the number of cards that can fit in the carousel at once
-    let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-
-    // Insert copies of the last few cards to beginning of carousel for infinite scrolling
-    carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-        carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-    });
-
-    // Insert copies of the first few cards to end of carousel for infinite scrolling
-    carouselChildrens.slice(0, cardPerView).forEach(card => {
-        carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-    });
-
-    // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-    carousel.classList.add("no-transition");
-    carousel.scrollLeft = carousel.offsetWidth;
-    carousel.classList.remove("no-transition");*/
-
-    // Add event listeners for the arrow buttons to scroll the carousel left and right
-    /*arrowBtns.forEach(btn => {
-        btn.addEventListener("click", () => {
-            carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-        });
-    });
-
-    // Dragging of the cards
-    const dragStart = (e) => {
-        isDragging = true;
-        carousel.classList.add("dragging");
-        // Records the initial cursor and scroll position of the carousel
-        startX = e.pageX;
-        startScrollLeft = carousel.scrollLeft;
-    }
-
-    const dragging = (e) => {
-        if(!isDragging) return; // if isDragging is false return from here
-        // Updates the scroll position of the carousel based on the cursor movement
-        carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-    }
-
-    const dragStop = () => {
-        isDragging = false;
-        carousel.classList.remove("dragging");
-    }*/
-
-    /*const infiniteScroll = () => {
-        // If the carousel is at the beginning, scroll to the end
-        if(carousel.scrollLeft === 0) {
-            carousel.classList.add("no-transition");
-            carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-            carousel.classList.remove("no-transition");
-        }
-        // If the carousel is at the end, scroll to the beginning
-        else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-            carousel.classList.add("no-transition");
-            carousel.scrollLeft = carousel.offsetWidth;
-            carousel.classList.remove("no-transition");
-        }
-
-        // Clear existing timeout & start autoplay if mouse is not hovering over carousel
-        clearTimeout(timeoutId);
-        if(!wrapper.matches(":hover")) autoPlay();
-    }
-
-    const autoPlay = () => {
-        if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-        // Autoplay the carousel after every 2500 ms
-        timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-    }
-    autoPlay();*/
-
-    //carousel.addEventListener("mousedown", dragStart);
-    //carousel.addEventListener("mousemove", dragging);
-    //document.addEventListener("mouseup", dragStop);
-    //carousel.addEventListener("scroll", infiniteScroll);
-    //wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-    //wrapper.addEventListener("mouseleave", autoPlay);
-
-    /*const updateButtonVisibility = () => {
-        const isAtFirstElement = carousel.scrollLeft === 0;
-        const isAtLastElement = Math.ceil(carousel.scrollLeft) >= carousel.scrollWidth - carousel.offsetWidth;
-
-        if (isAtFirstElement) {
-            arrowBtns[0].style.display = "none"; // Hide left button
-            arrowBtns[1].style.display = "block"; // Show right button
-        } else if (isAtLastElement) {
-            arrowBtns[0].style.display = "block"; // Show left button
-            arrowBtns[1].style.display = "none"; // Hide right button
-        } else {
-            arrowBtns.forEach(btn => {
-                btn.style.display = "block"; // Show both buttons
-            });
-        }
-    };
-
-    carousel.addEventListener("mousedown", () => {
-        isDragging = true;
-    });
-
-    carousel.addEventListener("mousemove", () => {
-        if (isDragging) {
-            updateButtonVisibility();
-        }
-    });
-
-    document.addEventListener("mouseup", () => {
-        isDragging = false;
-        updateButtonVisibility();
-    });
-
-    carousel.addEventListener("scroll", updateButtonVisibility);*/
-
+    // SwiperJs initialization
     const swiper = new Swiper('.swiper', {
         slidesPerView: 1,
         spaceBetween: 40,
@@ -182,25 +45,34 @@ document.addEventListener('DOMContentLoaded', function() {
             el: '.swiper-pagination',
             dynamicBullets: true
         },
-      
+    
         // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
     });
-      
 
-    // Get the hamburger and menu elements
+    // Portfolio-items' p and ul
+    const overflowingElements = document.querySelectorAll('.fade-overflow');
+
+    overflowingElements.forEach(function(element) {
+        element.addEventListener('scroll', function() {
+            setClasses(this);
+        });
+        
+        // Set initial classes for each overflowing element
+        setClasses(element);
+    });
+
+    // Navigation bar
     const hamburger = document.getElementById('hamburger');
     const menu = document.querySelector('.menu');
 
-    // Attach a click event listener to the hamburger element
     hamburger.addEventListener('click', function() {
         toggleOpen(this, menu);
     });
 
-    // Attach a click event listener to the menu element
     menu.addEventListener('click', function(event) {
         // Check if the clicked element is a link inside the menu
         if (event.target.tagName === 'A') {
